ac95b03ca5b8ebe1c4b99aacca659294c3640cb0
Anonymous <>
2023-03-07 22:26:22 +0530

    host

--- qtify-backend/index.js
+++ qtify-backend/index.js
@@ -1,4 +1,5 @@
 const fastify = require("fastify")({ logger: true });
+const port = process.env.PORT || 3001;
 
 // Declare a route
 fastify.get("/", async (request, reply) => {
@@ -8,7 +9,7 @@ fastify.get("/", async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
+    await fastify.listen({ port, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
