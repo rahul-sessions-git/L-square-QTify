9b2688bfe903e299a24aed3939a50c0070bea64c
Anonymous <>
2023-03-13 09:52:40 +0530

    backend code

--- qtify-backend/index.js
+++ qtify-backend/index.js
@@ -8,14 +8,13 @@ const cors = require("cors"); //import cors module
 const { generateName, randomInteger, GENRES } = require("./helpers/helpers");
 const { generateAlbums, generateData } = require("./data/albums");
 const { sampleSize } = require("lodash");
-// const chalk = require("chalk");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 const limiter = rateLimit({
-  windowMs: (1 * 60 * 1000) / 2, // 1 minute
-  max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 10 * 60 * 1000, // 1 minute
+  max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
@@ -46,30 +45,7 @@ app.use(
   })
 );
 
-var whitelist = ["localhost"]; //white list consumers
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "device-remember-token",
-    "Access-Control-Allow-Origin",
-    "Origin",
-    "Accept",
-  ],
-};
-app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
-
+app.use(cors()); //adding cors middleware to the express with above configurations
 app.use(limiter);
 
 // ---------------------- API ROUTES ------------------------
@@ -109,16 +85,6 @@ app.get("/songs", (req, res) => {
 app.get("/genres", (req, res) => {
   res.setHeader("Cache-Control", "public, max-age=600");
   res.json({ data: GENRES });
-});
-
-app.get("/hello", (req, res) => {
-  const response = {
-    name: req.query.name,
-    email: req.query.email,
-  };
-  console.log("\x1b[36m", `${req.query.name} ${req.query.email}`, "\x1b[0m");
-  res.setHeader("Cache-Control", "no-cache");
-  res.json({ response, message: `Well done ${req.query.name}` });
 });
 
 app.get("/faq", (req, res) => {
